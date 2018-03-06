e345a1068bd47b2f3602e49d2b6420024e9e8747
Annie Yin <wen.yin@duke.edu>
2018-03-06 01:09:48 -0500

    fixed addList, added login + signup

--- trello/js/trello.js
+++ trello/js/trello.js
@@ -5,13 +5,16 @@
  */
 
 var lists = [];
+var users = [];
 
 var app = new Vue ({
     el: '#app',
     data: {
         listData: lists,
+        userData: users,
         newCard: {name: '', description: '', deadline: '', id: null},
-        newList: {name: '', cards: [], id: null}
+        newList: {name: '', cards: [], id: null},
+        newUser: {name: '', email: '', id: null}
     },
     
     methods: {
@@ -20,10 +23,9 @@ var app = new Vue ({
             var modal = $('#add-modal');
             modal.css('display', 'block');
             var list = event.target.parentElement.getAttribute('id');
-            console.log(list);
             
-            $('#add-close').off('click');
-            $('#add-close').click(function () {
+            $('.close').off('click');
+            $('.close').click(function () {
                 modal.css('display', 'none');
             });
             
@@ -39,26 +41,23 @@ var app = new Vue ({
                 $('#add-modal').get(0).reset();
                 self.newCard = {name: '', description: '', deadline: '', id: null};
             });
-        }, 
-        
-        showCard: function (event) {
+        },
+    
+        showCard: function () {
             var self = this;
             var modal = $('#show-modal');
             modal.css('display', 'block');
             var card = parseInt(event.currentTarget.getAttribute('id'));
             var list = parseInt(event.currentTarget.parentElement.getAttribute('id'));
-        
-            console.log(card);
-            console.log(list);
-            
-            $('#show-close').off('click');
-            $('#show-close').click(function () {
+
+            $('.close').off('click');
+            $('.close').click(function () {
                 modal.css('display', 'none');
             });
             
             $('#show-name').text(self.listData[list].cards[card].name);
             $('#show-description').text(self.listData[list].cards[card].description);
-            $('#show-deadline').text(self.listData[list].cards[card].deadline);
+            $('#show-deadline').text('Deadline: ' + self.listData[list].cards[card].deadline);
         },
         
         deleteCard: function (event) {
@@ -74,44 +73,98 @@ var app = new Vue ({
         },
         
         addList: function () {
-            var self = this;
-            var listInput = $('<input id="list-input" v-model="newList.name" v-on:keyup.enter="submitList"/>');
-            listInput.css('margin-top', '2.5%');
-            listInput.css('margin-left', '2%');
-            listInput.css('height', '20%');
-            $('#add-list').replaceWith(listInput);
-            
-            /*$('#list-input').on("keydown", function(e) {
-                if (e.keyCode == 13) {
-                    console.log(self.newList.name);
-                    self.newList.name = self.newList.name.trim();
-                    self.newList.id = self.listData.length;
-                    if (self.newList.name) {
-                        self.listData.push(self.newList);
-                    }
-
-                    $('#listInput').replaceWith(addList);
-                    self.newList = {name: '', cards: [], id: null};
-                    return false;
-                }
-            });*/
+            var self = this;     
+            self.newList.name = self.newList.name.trim();
+            self.newList.id = self.listData.length;
+            if (self.newList.name) {
+                self.listData.push(self.newList);
+            }
+            $('#list-input').val("");
+            self.newList = {name: '', cards: [], id: null};
+                
         },
         
-        submitList: function () {
+        addUser: function () { 
             var self = this;
-            var addButton = $('#add-list');
-            addButton.append('<i class="fa fa-plus-circle"></i><h5 v-on:click="addList">Add list</h5>');
+            var modal = $('#add-user-modal');
+            modal.css('display', 'block');
             
-            console.log(self.newList.name);
+            $('.close').off('click');
+            $('.close').click(function () {
+                modal.css('display', 'none');
+            });
+            
+            $('#save-user').off('click');
+            $('#save-user').click(function () {
+                modal.css('display', 'none');
+                self.newUser.name = self.newUser.name.trim();
+                self.newUser.email = self.newUser.email.trim();
+                self.newUser.id = self.userData.length;
+                if (self.newUser.name && self.newUser.email) {
+                    self.userData.push(self.newUser);
+                }
+                
+                $('#add-user-modal').get(0).reset();
+                self.newUser = {name: '', email: '', id: null};
+            });
+        },
+        
+        checkUser: function () {
+            var self = this;
+            var modal = $('#sign-in-modal');
+            modal.css('display', 'block');
+            
+            $('.close').off('click');
+            $('.close').click(function () {
+                modal.css('display', 'none');
+            });
+            
+            var alreadyUser = false;
+            
+            $('#sign-in').off('click');
+            $('#sign-in').click(function () {
+                self.newUser.name = self.newUser.name.trim();
+                self.newUser.email = self.newUser.email.trim();
+                if (self.newUser.name && self.newUser.email) {
+                    for (var i = 0; i < self.userData.length; i++) {
+                        if (self.userData[i].name == self.newUser.name && self.userData[i].email == self.newUser.email) {
+                            modal.css('display', 'none');
+                            alreadyUser = true;
+                            break;
+                        }
+                    }
+                    
+                    if (alreadyUser == false) {
+                        var error = $('<p id="error">User not found. Please sign up!</p>');
+                        error.css('text-align', 'center');
+                        error.css('color', 'darkgray');
+                        error.css('font-weight', 'bold');
+                        $('#sign-in').replaceWith(error);
+                        
+                        $('.close').off('click');
+                        $('.close').click(function () {
+                            $('#error').replaceWith($('<p id="sign-in" class="save">Sign in</p>'))
+                            modal.css('display', 'none');
+                        });
+                    }
+                } 
+                
+                $('#sign-in-modal').get(0).reset();
+                self.newUser = {name: '', email: '', id: null};
+            });
+        }
+        
+        /* editCardName: function (event) {
+            
+        },
+        
+        editListName: function (event) {
+            
         },
         
         deleteList: function () {
             
-        },
-        
-        addUser: function () {
-            
-        }
+        } */
     },
     
     mounted: function () {
